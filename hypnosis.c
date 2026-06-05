afa51abaa119598bc8c18c81f7aaa33908de98c9
chrnz008 <charancuz008@gmail.com>
2026-06-05 20:19:59 +0530

    got a basic working structure

--- hypnosis.c
+++ hypnosis.c
@@ -1,5 +1,11 @@
 #include <raylib.h>
 
+#define WAVE_NO 100
+#define WAVE_SPACING 10
+#define WAVE_SPEED 1
+
+int waves[WAVE_NO]; // each wave contains radius
+
 int main() {
 
     const int window_width = 800;
@@ -9,8 +15,29 @@ int main() {
     ClearBackground(BLACK);
     HideCursor();
 
+    const int src_rad = 5;
+
+    struct Vector2 center = {window_width / 2, window_height / 2};
+
     while (!WindowShouldClose()) {
         BeginDrawing();
+
+        DrawCircleV(center, src_rad, WHITE);
+
+        int idx = 0;
+        do {
+
+            if ( waves[idx] > window_width)
+            	waves[idx] = 10;
+
+            DrawCircleLinesV(center, waves[idx], WHITE);
+            waves[idx] += 10; // increment wave radius
+
+			idx++;
+
+        } while (waves[idx] - 10 != 0 && idx < WAVE_NO); // till next child wave 
+														// (-1 because we are incrementing previously)
+
         EndDrawing();
     }
 
